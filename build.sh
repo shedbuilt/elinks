67f38fc40ec817371f7b191602ea1eaa98c81b79
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:02:59 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 sed -i 's/CONFIG_88_COLORS=.*/CONFIG_88_COLORS=yes/g' features.conf && \
 sed -i 's/CONFIG_256_COLORS=.*/CONFIG_256_COLORS=yes/g' features.conf && \
 ./configure --prefix=/usr && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
